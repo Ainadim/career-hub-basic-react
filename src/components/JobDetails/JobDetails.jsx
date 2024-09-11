89608aac4d9c8085a108355f7e8ed82d657a2ab6
Arafin Islam <nadimface@gmailcom>
2024-09-11 23:39:07 +0600

    add

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useLoaderData, useParams } from 'react-router-dom';
 
 const JobDetails = props => {
+    const jobs = useLoaderData()
+    const {id} = useParams()
+    const idNum = parseInt(id)
+    const job = jobs.find(job => job.id === idNum)
+    console.log(job);
     return (
         <div>
             <h2>Here is the Job detail page</h2>
+            
         </div>
     );
 };
